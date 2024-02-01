7e2af5b73834277bddb8313d89c22855032a5dfb
Ghariani Islem <islem24762048@gmail.com>
2024-02-01 17:37:45 +0100

    first commit

---
+++ Backend_SpringBoot/src/main/java/com/ghirr/LuxeRide/LuxeRideApplication.java
@@ -0,0 +1,13 @@
+package com.ghirr.LuxeRide;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuxeRideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuxeRideApplication.class, args);
+	}
+
+}
